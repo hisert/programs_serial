3975ba75566452f8ee62945bc08cf5b3aa18efee
hisert <100667448+hisert@users.noreply.github.com>
2024-04-02 11:09:22 +0300

    Update h_serial.py

--- h_serial.py
+++ h_serial.py
@@ -31,7 +31,7 @@ class MySerialPort:
             
     def send_string(self, data):
         if self.ser and self.ser.is_open:
-            self.ser.write(data.decode())
+            self.ser.write(data.encode())
             
     def close(self):
         if self.ser and self.ser.is_open:
