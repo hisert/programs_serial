231e01dd14eebdb770804719bc905e9f4b188cd6
hisert <100667448+hisert@users.noreply.github.com>
2024-04-02 10:51:42 +0300

    Update h_serial.py

--- h_serial.py
+++ h_serial.py
@@ -6,14 +6,13 @@ import sys
 
 class MyServer:
   
-    def __init__(self, host, port, backlog, custom_function=None, stop_function=None):
+    def __init__(self, host, port, backlog, custom_function=None):
         self.host = host
         self.port = port
         self.backlog = backlog
         self.server_socket = None
         self.running = False
         self.custom_function = custom_function
-        self.stop_function = stop_function
         self.client_sockets = []  # Bağlı olan tüm client soketlerini saklamak için liste
 
     def start(self):
@@ -31,8 +30,6 @@ class MyServer:
             self.server_socket.close()
         if self.server_thread:
             self.server_thread.join()
-        if self.stop_function:  # Stop fonksiyonu varsa çağır
-            self.stop_function()
 
     def server_loop(self):
         while self.running:
@@ -62,17 +59,17 @@ class MyServer:
                 client_socket.sendall(message.encode())
             except Exception as e:
                 print("Error sending message to client:", e)
+              
+
 
 def data_arrived(data):
+    global server
     received_message = data.replace('(', '<')
     received_message = received_message.replace(')', '>')
     print(received_message)
-
-def stop_func():
-    print("BY BY")
-
-server = MyServer('0.0.0.0', 12349, 5, custom_function=data_arrived, stop_function=stop_func)        
-
+    server.send(received_message)
+  
+server = MyServer('0.0.0.0', 12349, 5, custom_function=data_arrived)        
 def main():
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     server.start()
