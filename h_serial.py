9021990f7a89b7b1687ac3fa036f18b720c77ac1
hisert <100667448+hisert@users.noreply.github.com>
2024-04-02 11:05:13 +0300

    Update h_serial.py

--- h_serial.py
+++ h_serial.py
@@ -1,5 +1,4 @@
 import serial
-import threading
 import time
 
 class MySerialPort:
@@ -10,26 +9,23 @@ class MySerialPort:
         self.timeout = timeout
         self.ser = None
         self.buffer = bytearray()  # Veriyi tutmak için bir buffer
-        self.gelen = "" # Veriyi tutmak için bir buffer
     
     def open(self):
         self.ser = serial.Serial(self.port, self.baudrate, timeout=self.timeout)
     
     def read_data(self):
         if self.ser:
-            while True:
-                byte = self.ser.read(1)
-                if byte:
-                    if byte == b'<':
-                        self.buffer.clear()
-                    self.buffer.extend(byte)
-                    if byte == b'>':
-                        data = self.buffer.decode().strip()
-                        self.gelen = data
-                        self.buffer.clear()  # Bufferi temizle
-                        return self.gelen
-                else:
-                    return ""
+            byte = self.ser.read(1)
+            if byte:
+                if byte == b'<':
+                    self.buffer.clear()
+                self.buffer.extend(byte)
+                if byte == b'>':
+                    data = self.buffer.decode().strip()
+                    self.buffer.clear()  # Bufferi temizle
+                    return data
+            else:
+                return ""
         else:
             return ""
             
@@ -41,27 +37,28 @@ class MySerialPort:
         if self.ser and self.ser.is_open:
             self.ser.close()
 
-def send_data(serial_port):
-    while True:
-        serial_port.send_string("<Q00000001:0000>")
-        time.sleep(1)
-
 def main():
-    port = "/dev/ttyS1"  # Seri port adı
+    port = '/dev/ttyS1'  # Seri port adı
     baudrate = 9600  # Aktarılan bit hızı
     timeout = 1  # Okuma zaman aşımı
 
     serial_port = MySerialPort(port, baudrate, timeout)
     serial_port.open()
 
-    send_thread = threading.Thread(target=send_data, args=(serial_port,))
-    send_thread.start()
+    last_send_time = time.time()
 
     try:
         while True:
+            # Veri gönderme
+            current_time = time.time()
+            if current_time - last_send_time >= 1:
+                serial_port.send_string("<Q00000001:0000>")
+                last_send_time = current_time
+            
+            # Veri alma
             data = serial_port.read_data()
             if data:
-                print("Received:", data)
+                print("Received:", data)  # Gelen veriyi ekrana yazdır
     except KeyboardInterrupt:
         serial_port.close()
 
