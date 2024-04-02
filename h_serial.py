84df91025c286e4d8dc8fe76a441662fa537e055
hisert <100667448+hisert@users.noreply.github.com>
2024-04-02 09:30:43 +0300

    Update h_serial.py

--- h_serial.py
+++ h_serial.py
@@ -1,3 +1,4 @@
+User
 import socket
 import threading
 import signal
@@ -11,8 +12,6 @@ class MyServer:
         self.port = port
         self.backlog = backlog
         self.server_socket = None
-        self.client_sockets = []  # Bağlı olan tüm clientlerin soketlerini saklamak için bir liste
-        self.client_threads = []  # Bağlı olan tüm clientlerin thread'lerini saklamak için bir liste
         self.running = False
 
     def start(self):
@@ -31,22 +30,12 @@ class MyServer:
         if self.server_thread:
             self.server_thread.join()
 
-        # Bağlı olan tüm clientleri ve ilgili thread'leri kapat
-        for client_socket in self.client_sockets:
-            client_socket.close()
-        for client_thread in self.client_threads:
-            client_thread.join()
-
     def server_loop(self):
         while self.running:
             try:
                 client_socket, client_address = self.server_socket.accept()
-                # Bağlı olan client soketini listeye ekle
-                self.client_sockets.append(client_socket)
                 client_thread = threading.Thread(target=self.handle_client, args=(client_socket, client_address))
                 client_thread.start()
-                # Bağlı olan client thread'ini listeye ekle
-                self.client_threads.append(client_thread)
             except KeyboardInterrupt:
                 self.stop()
                 break
